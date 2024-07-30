952ccafc35e5499de47afa4dc71cc936c83905f5
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-07-30 15:34:35 -0300

    aula04

---
+++ POO/Aula04/src/aula04/Aula04.java
@@ -0,0 +1,9 @@
+package aula04;
+public class Aula04 {
+    public static void main(String[] args) {
+       Caneta c1 = new Caneta("NIC", 0.04f, "Amarela");
+      c1.status();
+      Caneta c2 = new Caneta("BIC", 0.5f, "Preta");
+      c2.status();
+    }
+}
