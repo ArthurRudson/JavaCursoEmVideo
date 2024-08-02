89efc9025bce4967c93b74ac48f53076d3b349ae
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-08-02 18:53:56 -0300

    aula09

---
+++ POO/aula09/src/aula09/Aula09.java
@@ -0,0 +1,21 @@
+package aula09;
+public class Aula09 {
+    public static void main(String[] args) {
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+        
+        p[0] = new Pessoa("Predro", 22, "M");
+        p[1] = new Pessoa("Maria", 25, "F");
+        
+        l[0] = new Livro("Aprendendo Java", "José da silva", 300, p[0]);
+        l[1] = new Livro("POO para Iniciantes", "Pedro Paulo", 500, p[1]);
+        l[2] = new Livro("Java Avancado", "Maria Candido", 800, p[0]);
+        
+        l[0].abrir();
+        l[0].folhear(200);
+        l[0].avancarPag();
+        
+        System.out.println(l[0].detalhes());
+    }
+    
+}
