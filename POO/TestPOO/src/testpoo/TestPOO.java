fdb816946653e1e73e1de993ab747e2e2583b961
ArthurRudson <arthurrudson@gmail.com>
2024-09-12 14:48:02 -0300

    add

--- POO/TestPOO/src/testpoo/TestPOO.java
+++ POO/TestPOO/src/testpoo/TestPOO.java
@@ -1,7 +1,22 @@
 package testpoo;
+
+import java.util.Scanner;
+
 public class TestPOO {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			double div = (double) a / b;
+			if (b != 0) {	
+				System.out.println(div);
+			} else {
+				System.out.println("divisao imposivel");
+			} 
+		}
+		
     }
     
 }
